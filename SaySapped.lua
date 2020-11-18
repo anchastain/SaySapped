8ea30d07340e5aceacfe7952d8cf78f5d24dc37a
Aurae <anchastain@gmail.com>
2020-11-18 13:58:48 -0600

    Updates

--- SaySapped.lua
+++ SaySapped.lua
@@ -1,14 +1,19 @@
 local SaySapped = CreateFrame("Frame")
 SaySapped.playername = UnitName("player")
 
-SaySapped:SetScript("OnEvent", function(_,_,_,event, _,_,sourceName, _,_,_,destName, _,_,spellId)
-	if ((spellId == 6770)
-	and (destName == SaySapped.playername)
-	and (event == "SPELL_AURA_APPLIED" or event == "SPELL_AURA_REFRESH"))
-	then
-		SendChatMessage("Sapped by: "..(sourceName or "(unknown)"),"SAY")
-		DEFAULT_CHAT_FRAME:AddMessage("Sapped by: "..(sourceName or "(unknown)"))
-	end
+SaySapped:SetScript("OnEvent", function(self, evt)
+    if( evt == "COMBAT_LOG_EVENT_UNFILTERED" )then
+        local _, event, _, _, sourceName, _, _, _, dstName, _,_,spellId = CombatLogGetCurrentEventInfo()
+
+        if( (spellId == 6770)
+            and (dstName == SaySapped.playername)
+            and (event == "SPELL_AURA_APPLIED" or event == "SPELL_AURA_REFRESH")
+            )then
+            SendChatMessage("Sapped by: "..(sourceName or "(unknown)"),"SAY")
+            DEFAULT_CHAT_FRAME:AddMessage("Sapped by: "..(sourceName or "(unknown)"))
+        end
+
+    end
 end)
 
 SaySapped:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
